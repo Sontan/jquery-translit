3b87bebac9c84cb3d5ab16961d90891b82180422
Vadim Borodin <borodean@gmail.com>
2012-12-04 04:50:08 +0200

    v0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,74 @@
 jquery-translit
 ===============
 
-jquery-translit
+installation
+------------
+
+Include script *after* the jQuery library:
+
+```html
+<script src="/path/to/jquery.translit-0.1.0.js"></script>
+```
+
+usage
+-----
+
+Replace the existing values of the elements with transliterated ones:
+
+```javascript
+// $('input').val() -> 'Официант, 400 капель валерьянки и салат!'
+$('input').translit();
+// $('input').val() -> '"Ofitciant, 400 kapel valerianki i salat!"'
+```
+
+Change the value of another element with the transliterated value of this
+one:
+
+```javascript
+// $('input#input-1').val() -> 'Мы не бандиты! Мы благородные пираты!'
+// $('input#input-2').val() -> ''
+$('input#input-1').translit('send', 'input#input-2');
+// $('input#input-1').val() -> 'Мы не бандиты! Мы благородные пираты!'
+// $('input#input-2').val() -> 'My ne bandity! My blagorodnye piraty!'
+```
+
+Change the value of the this element with the transliterated value of another
+one:
+
+```javascript
+// $('input#input-1').val() -> ''
+// $('input#input-2').val() -> 'Если коровы начнут летать, то мне в космосе
+//                              делать нечего'
+$('input#input-1').translit('receive', 'input#input-2');
+// $('input#input-1').val() -> 'Esli korovy nachnut letat, to mne v kosmose
+//                              delat nechego'
+// $('input#input-2').val() -> 'Если коровы начнут летать, то мне в космосе
+//                              делать нечего'
+```
+
+Watch the value of another element and change the value of current one with
+it's transliterated equivalent on-the-fly:
+
+```javascript
+$('input#input-1').translit('watch', 'input#input-2');
+```
+
+Same as above, but stop watching if value of the current element is changed
+manually:
+
+```javascript
+$('input#input-1').translit('watch', 'input#input-2', true);
+```
+
+Stop watching:
+
+```javascript
+$('input#input-1').translit('unwatch');
+```
+
+Replace the existing values of the elements with transliterated ones:
+
+```javascript
+$.fn.translit('exec', 'Прица Говорун отличается умом и сообразительностью')
+// -> "Pritca Govorun otlichaetsia umom i soobrazitelnostiu"
+```
---
+++ jquery.translit-0.1.0.js
@@ -0,0 +1,113 @@
+/**
+ * jQuery Translit v0.1.0
+ * Vadim Borodean, 2012
+ */
+
+/*global jQuery */
+
+(function ($) {
+
+    'use strict';
+
+    var dictionary, events, methods;
+
+    dictionary = {
+        'а': 'a',
+        'б': 'b',
+        'в': 'v',
+        'г': 'g',
+        'д': 'd',
+        'е': 'e',
+        'ё': 'e',
+        'ж': 'zh',
+        'з': 'z',
+        'и': 'i',
+        'й': 'i',
+        'к': 'k',
+        'л': 'l',
+        'м': 'm',
+        'н': 'n',
+        'о': 'o',
+        'п': 'p',
+        'р': 'r',
+        'с': 's',
+        'т': 't',
+        'у': 'u',
+        'ф': 'f',
+        'х': 'kh',
+        'ц': 'tc',
+        'ч': 'ch',
+        'ш': 'sh',
+        'щ': 'shch',
+        'ъ': '',
+        'ы': 'y',
+        'ь': '',
+        'э': 'e',
+        'ю': 'iu',
+        'я': 'ia'
+    };
+
+    events = 'input.translit keyup.translit propertychange.translit';
+
+    methods = {
+        init: function () {
+            return this.each(function () {
+                $(this).val(methods.exec($(this).val()));
+            });
+        },
+        exec: function (string) {
+            return string.replace(/([а-яё])/gi, function (matchResult) {
+                var matchResultLowerCase, replaceValue;
+                matchResultLowerCase = matchResult.toLowerCase();
+                replaceValue = dictionary[matchResultLowerCase];
+                if (typeof replaceValue === 'undefined') {
+                    return matchResult;
+                }
+                if (matchResultLowerCase === matchResult) {
+                    return replaceValue;
+                }
+                return replaceValue.substring(0, 1).toUpperCase() +
+                       replaceValue.substring(1);
+            });
+        },
+        receive: function (sender) {
+            $(this).val(methods.exec($(sender).val()));
+        },
+        send: function (receiver) {
+            $(receiver).val(methods.exec($(this).val()));
+        },
+        watch: function (sender, unwatchOnChange) {
+            return this.each(function () {
+                var data, that;
+                data = $(this).data('senders');
+                that = this;
+                $(this).data('senders', $(data).add(sender));
+                $(sender).on(events, function (e) {
+                    $(this).translit('send', that);
+                });
+                if (unwatchOnChange) {
+                    $(this).one('change.translit', function (e) {
+                        $(this).translit('unwatch');
+                    });
+                }
+            });
+        },
+        unwatch: function () {
+            return this.each(function () {
+                $($(this).data('senders')).off(events);
+                $(this).removeData('senders');
+            });
+        }
+    };
+
+    $.fn.translit = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        }
+        if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        }
+        $.error('Method ' + method + ' does not exist on jQuery.translit');
+    };
+
+}(jQuery));
